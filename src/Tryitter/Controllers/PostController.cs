a16f600ce39bda93df776c6a3e6e337566b0e494
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-12-06 19:05:30 +0000

    feat: add validation to controller

--- src/Tryitter/Controllers/PostController.cs
+++ src/Tryitter/Controllers/PostController.cs
@@ -20,7 +20,7 @@ namespace Tryitter.WebApi.Controllers
         [Authorize(policy: "Student")]
         public async Task<IActionResult> GetOne(Guid id)
         {
-            var studentId = GetGuidToken();
+            var studentId = new Guid(User.Identity!.Name!);
 
             return Ok(await _postServices.GetPostById(id, studentId));
         }
@@ -29,29 +29,60 @@ namespace Tryitter.WebApi.Controllers
         [Authorize(policy: "Student")]
         public async Task<IActionResult> GetAll()
         {
-            var studentId = GetGuidToken();
+            var studentId = new Guid(User.Identity!.Name!);
 
             return Ok(await _postServices.GetAllPost(studentId));
         }
 
         [HttpPost]
         [Authorize(policy: "Student")]
-        public async Task<IActionResult> Create(Post toCreate)
+        public async Task<IActionResult> Create(Post postBody)
         {
-            var studentId = GetGuidToken();
+            var studentId = new Guid(User.Identity!.Name!);
 
-            var post = await _postServices.CreatePost(toCreate, studentId);
+            var postToCreate = new Post(postBody.Message);
+
+            if (!postToCreate.IsValid)
+            {
+                var keyErrors = postToCreate.Notifications.Aggregate("",
+                    (current, studentNotification) => current + (studentNotification.Key + ", "));
+                var errorsMessage = postToCreate.Notifications.Aggregate("",
+                    (current, studentNotification) => current + (studentNotification.Message + ", "));
+
+                return Problem(
+                    title: keyErrors,
+                    statusCode: 400,
+                    detail: errorsMessage);
+            }
+
+            var post = await _postServices.CreatePost(postToCreate, studentId);
 
             return Created($"/post/{post.Id}", post.Id);
         }
+
         [HttpPut]
         [Authorize(policy: "Student")]
-        public async Task<IActionResult> Update(Post toUpdate)
+        public async Task<IActionResult> Update(Post postBody)
         {
-            var studentId = GetGuidToken();
+            var studentId = new Guid(User.Identity!.Name!);
 
-            var updatedPost = await _postServices.UpdatePost(toUpdate, studentId);
-            
+            var postToUpdate = new Post(postBody.Message);
+
+            if (!postToUpdate.IsValid)
+            {
+                var keyErrors = postToUpdate.Notifications.Aggregate("",
+                    (current, studentNotification) => current + (studentNotification.Key + ", "));
+                var errorsMessage = postToUpdate.Notifications.Aggregate("",
+                    (current, studentNotification) => current + (studentNotification.Message + ", "));
+
+                return Problem(
+                    title: keyErrors,
+                    statusCode: 400,
+                    detail: errorsMessage);
+            }
+
+            var updatedPost = await _postServices.UpdatePost(postToUpdate, studentId);
+
             return Ok(updatedPost);
         }
 
@@ -59,25 +90,11 @@ namespace Tryitter.WebApi.Controllers
         [Authorize(policy: "Student")]
         public async Task<IActionResult> Delete(Guid id)
         {
-            var studentId = GetGuidToken();
+            var studentId = new Guid(User.Identity!.Name!);
 
-            var isDeleted = await _postServices.DeletePost(id, studentId);
-
-            if (isDeleted is false)
-                return BadRequest();
+            await _postServices.DeletePost(id, studentId);
 
             return NoContent();
-        }
-
-        public Guid GetGuidToken()
-        {
-            if (User.Identity?.Name is null)
-            {
-                return Guid.Empty; // Ja existe, validacao aqui so para parar os Warnings
-            }
-
-            var id = new Guid(User.Identity.Name);
-            return id;
         }
     }
 }
